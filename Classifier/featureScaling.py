f333306d5ebe27452907598f797c0df5f3841cd7
Akim Delli <adelli@hearst.com>
2016-11-04 09:15:09 -0400

    fix feature scaling

---
+++ Classifier/featureScaling.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from adaline import AdalineGD
+from mainPerceptron import plot_decision_regions
+
+# Print the tail of the data
+df = pd.read_csv('http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None)
+
+# Visualisation the Data
+y = df.iloc[0:100, 4].values
+print(y)
+y = np.where(y == 'Iris-setosa', -1, 1)
+print(y)
+X = df.iloc[0:100, [0, 2]].values
+
+
+X_std = np.copy(X)
+X_std[:, 0] = (X[:, 0] - X[:, 0].mean()) / X[:, 0].std()
+X_std[:, 1] = (X[:, 1] - X[:, 1].mean()) / X[:, 1].std()
+
+ada = AdalineGD(n_iter=15, eta=0.01)
+ada.fit(X_std, y)
+plot_decision_regions(X_std, y, classifier=ada)
+plt.title('Adaline - Gradient descent')
+plt.xlabel('sepal length [standardized')
+plt.ylabel('petal length [standardized')
+plt.legend(loc='upper left')
+plt.show()
+plt.plot(range(1, len(ada.cost_) + 1), ada.cost_, marker='o')
+plt.xlabel('Epochs')
+plt.ylabel('Sum-squared-error')
+plt.show()
