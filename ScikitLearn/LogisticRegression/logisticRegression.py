7f3fb683d2e36d9101c878ffef3579ebe8864fea
akim <akim_delli@hotmail.com>
2016-11-12 01:36:51 -0500

    Logistic regretion with scikitLearn

--- ScikitLearn/LogisticRegression/logisticRegression.py
+++ ScikitLearn/LogisticRegression/logisticRegression.py
@@ -1,11 +1,11 @@
+from sklearn.linear_model import LogisticRegression
 from sklearn import datasets
 from sklearn.cross_validation import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import Perceptron
-from sklearn.metrics import accuracy_score
 from matplotlib.colors import ListedColormap
 import matplotlib.pyplot as plt
 import numpy as np
+
 
 iris = datasets.load_iris()
 X = iris.data[:, [2, 3]]
@@ -13,17 +13,16 @@ y = iris.target
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
 
+
 # Features scaling
 sc = StandardScaler()
 sc.fit(X_train)
 X_train_std = sc.transform(X_train)
 X_test_std = sc.transform(X_test)
 
-ppn = Perceptron(n_iter=40, eta0=0.1, random_state=0)
-ppn.fit(X_train_std, y_train)
-y_pred = ppn.predict(X_test_std)
-print('Misclassified samples: %d' % (y_test != y_pred).sum())
-print('Accuracy: %.2f' % accuracy_score(y_test, y_pred))
+
+lr = LogisticRegression(C=1000.0, random_state=0)
+lr.fit(X_train_std, y_train)
 
 
 def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
@@ -55,11 +54,13 @@ def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
 
 X_combined_std = np.vstack((X_train_std, X_test_std))
 y_combined = np.hstack((y_train, y_test))
-plot_decision_regions(X=X_combined_std,
-                      y=y_combined,
-                      classifier=ppn,
-                      test_idx=range(105, 150))
+
+plot_decision_regions(X_combined_std, y_combined, classifier=lr, test_idx=range(105, 150))
+
 plt.xlabel('label length [standardized]')
 plt.xlabel('label width [standardized]')
 plt.legend(loc='upper left')
 plt.show()
+
+prediction = lr.predict_proba(X_test_std[0, :])
+print(prediction)
