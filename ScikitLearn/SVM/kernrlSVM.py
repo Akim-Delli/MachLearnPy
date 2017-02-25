f1e8de05ddae3ba5e4cacbcbfe7f09c85cdd0e1c
akim <akim_delli@hotmail.com>
2017-02-24 23:05:36 -0500

    Kernel SVM plot with big gamma

--- ScikitLearn/SVM/kernrlSVM.py
+++ ScikitLearn/SVM/kernrlSVM.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.svm import SVC
+from ScikitLearn.SVM.svm import plot_decision_regions
 
 np.random.seed(0)
 X_xor = np.random.randn(200, 2)
@@ -10,4 +12,17 @@ plt.scatter(X_xor[y_xor == 1, 0], X_xor[y_xor == 1, 1], c='b', marker='x', label
 plt.scatter(X_xor[y_xor == -1, 0], X_xor[y_xor == -1, 1], c='r', marker='s', label='-1')
 plt.ylim(-3.0)
 plt.legend()
+plt.show()
+
+svm = SVC(kernel='rbf', random_state=0, gamma=0.10, C=10.0)
+svm.fit(X_xor, y_xor)
+plot_decision_regions(X_xor, y_xor, classifier=svm)
+plt.legend(loc='upper left')
+plt.show()
+
+# gamma = 100
+svm = SVC(kernel='rbf', random_state=0, gamma=100, C=10.0)
+svm.fit(X_xor, y_xor)
+plot_decision_regions(X_xor, y_xor, classifier=svm)
+plt.legend(loc='upper left')
 plt.show()
