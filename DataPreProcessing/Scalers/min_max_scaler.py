f5d7a2a6382dccbde781fc52f74a7e88724608c5
akim <akim_delli@hotmail.com>
2017-05-07 16:23:15 -0400

    Scalers: Normalisation & Standardisation

---
+++ DataPreProcessing/Scalers/min_max_scaler.py
@@ -0,0 +1,26 @@
+from sklearn.preprocessing import MinMaxScaler
+import pandas as pd
+import numpy as np
+from sklearn.cross_validation import train_test_split
+
+df_wine = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data')
+df_wine.columns = ['Class label', 'Alcohol',
+                   'Malic acid', 'Ash',
+                   'Alcalinicity of ash', 'Magnesium',
+                   'Total phenols', 'Flavonoids',
+                   'Nonflavanoid phenols',
+                   'Proanthocyanins',
+                   'Color intensity', 'Hue',
+                   'OD280/OD315 of Dilutes wines',
+                   'Proline']
+
+print('Class labels', np.unique(df_wine['Class label']))
+print(df_wine.head())
+
+X, y = df_wine.iloc[:, 1:].values, df_wine.iloc[:, 0].values
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+# Min Max Scaler : Normalisation
+mms = MinMaxScaler()
+X_train_norm = mms.fit_transform(X_train)
+X_test_norm = mms.transform(X_test)
